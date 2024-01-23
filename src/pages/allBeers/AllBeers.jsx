d9da5ccfdf352ee723e2ac27e3ae01cd29144e7a
JohannaHaer <johanna.haerdter@gmail.com>
2024-01-23 12:33:31 +0100

    alles importiert und installiert

---
+++ src/pages/allBeers/AllBeers.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AllBeers = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default AllBeers
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <>
+        <img src="" alt="" />
+        <Link to='/allbeers'>All Beers</Link>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, consequuntur maiores illo molestiae labore pariatur deserunt ipsa ullam officia necessitatibus.</p>
+    </>
+  )
+}
+
+export default Home
