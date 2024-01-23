c8740af665228ceaeb028a186868c05af68b9fae
JohannaHaer <johanna.haerdter@gmail.com>
2024-01-23 20:46:49 +0100

    erste styles home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,21 +4,21 @@ import allbeers from '../../assets/img/allbeers.jpg'
 import randombeer from '../../assets/img/randombeer.png'
 
 const Home = () => {
-  return (
-    <main>
-        <h1>Beer Tap App</h1>
-        <section className='allbeers'>
-            <img src={allbeers} />
-            <Link to='/allbeers'>All Beers</Link>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, consequuntur maiores illo molestiae labore pariatur deserunt ipsa ullam officia necessitatibus.</p>
-        </section>
-        <section className='randombeer'>
-            <img src={randombeer} />
-            <Link to='/random'>Random Beer</Link>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, consequuntur maiores illo molestiae labore pariatur deserunt ipsa ullam officia necessitatibus.</p>
-        </section>
-    </main>
-  )
+    return (
+        <main>
+            <h1>Beer Tap App</h1>
+            <section className='allbeers'>
+                <img src={allbeers} />
+                <Link className='allbeersLink' to='/allbeers'>All Beers</Link>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, consequuntur maiores illo molestiae labore pariatur deserunt ipsa ullam officia necessitatibus.</p>
+            </section>
+            <section className='randombeer'>
+                <img src={randombeer} />
+                <Link className='randombeerLink' to='/random'>Random Beer</Link>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, consequuntur maiores illo molestiae labore pariatur deserunt ipsa ullam officia necessitatibus.</p>
+            </section>
+        </main>
+    )
 }
 
 export default Home
