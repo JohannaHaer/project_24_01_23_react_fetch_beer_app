b2ed9ffe2be795b3bf3b1561f712ca0761793f38
JohannaHaer <johanna.haerdter@gmail.com>
2024-01-23 23:00:37 +0100

    random fertig

--- src/components/randomBeer/RandomBeer.jsx
+++ src/components/randomBeer/RandomBeer.jsx
@@ -23,28 +23,28 @@ const RandomBeer = () => {
         <>
             {beers
             ? (
-                <div>
+                <div className='beerdetailsCard'>
                     <img src={beers.image_url} />
-                    <h2>{beers.name}</h2>
+                    <h2 className='h2Gross'>{beers.name}</h2>
                     <h3>{beers.tagline}</h3>
-                    <div>
+                    <div className='beerdetailsDiv'>
                         <div>
-                            <p>First brewed:</p>
-                            <p>Attenuation level:</p>
+                            <p className='pGray'>First brewed:</p>
+                            <p className='pGray'>Attenuation level:</p>
                         </div>
                         <div>
-                            <p>{beers.first_brewed}</p>
-                            <p>{beers.attenuation_level}</p>
+                            <p className='pGray'>{beers.first_brewed}</p>
+                            <p className='pGray'>{beers.attenuation_level}</p>
                         </div>
                     </div>
-                    <p>{beers.description}</p>
-                    <p>Contributed by: {beers.contributed_by.slice(0, beers.contributed_by.indexOf("<"))}</p>
+                    <p className='pBlack'>{beers.description}</p>
+                    <p className='pBlack pMargin'>Contributed by: {beers.contributed_by.slice(0, beers.contributed_by.indexOf("<"))}</p>
                 </div>
             )
             : (<p>Loading...</p>)
             }
-            <Link to='/allbeers'><img src={backLogo}/></Link>
-            <Link to='/random' onClick={() => window.location.reload()}><img src={againLogo}/></Link>
+            <Link to='/allbeers'><img className='beerdetailsLink' src={backLogo}/></Link>
+            <Link to='/random' onClick={() => window.location.reload()}><img className='randombeerImg' src={againLogo}/></Link>
             <Link className='allbeersLink allbeersCenter' to='/'><img src={beerLogo}/></Link>
         </>
     )
